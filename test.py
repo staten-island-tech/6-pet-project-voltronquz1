125148282a5b2eaac87d6161d8e98246521ff19f
a <justinc1127@nycstudents.net>
2025-11-12 09:53:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -12,15 +12,26 @@
 
 # Calculator.add(5, 6)
 
-class Hero:
-    def __init__(self, name, money, inventory):
-        self.name = name
-        self.money = money
-        self.inventory = inventory
+# class Hero:
+#     def __init__(self, name, money, inventory):
+#         self.name = name
+#         self.money = money
+#         self.inventory = inventory
 
-    def buy(self, item):
-        self.inventory.append(item)
-        print(self.inventory)
-Jillian = Hero("Jillian", 150, ["Potion"])
-Jillian.buy({"title": "Sword", "atk": 34})
-print(Jillian.__dict__)
+#     def buy(self, item):
+#         self.inventory.append(item)
+#         print(self.inventory)
+# Jillian = Hero("Jillian", 150, ["Potion"])
+# Jillian.buy({"title": "Sword", "atk": 34})
+# print(Jillian.__dict__)
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance  # double underscore means "private"
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
