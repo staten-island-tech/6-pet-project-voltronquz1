64c67c54b6bf98a2de9300ae53b4d4a3dbd01371
a <justinc1127@nycstudents.net>
2025-11-12 09:38:33 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,26 @@
-class Calculator():
-    def add(x, y):
-        print(x + y)
-        return x + y
+# class Calculator():
+#     def add(x, y):
+#         print(x + y)
+#         return x + y
 
-    def add_many(numbers):
-        print(sum(numbers))
-        return sum(numbers)
+#     def add_many(numbers):
+#         print(sum(numbers))
+#         return sum(numbers)
 
-    def subtract(numbers):
-        return numbers
+#     def subtract(numbers):
+#         return numbers
 
-Calculator.add(5, 6)
+# Calculator.add(5, 6)
+
+class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+Jillian = Hero("Jillian", 150, ["Potion"])
+Jillian.buy({"title": "Sword", "atk": 34})
+print(Jillian.__dict__)
