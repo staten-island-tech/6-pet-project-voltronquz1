cbf3d3d85c4ae21d4c64a708b5dddf19070a3b0c
a <justinc1127@nycstudents.net>
2025-11-19 10:16:23 -0500

    Update test.py

--- test.py
+++ test.py
@@ -12,11 +12,11 @@
 
 # Calculator.add(5, 6)
 
-class Hero:
-    def __init__(self, name, money, inventory):
-        self.name = name
-        self.money = money
-        self.inventory = inventory
+# class Hero:
+#     def __init__(self, name, money, inventory):
+#         self.name = name
+#         self.money = money
+#         self.inventory = inventory
 
 #     def buy(self, item):
 #         self.inventory.append(item)
@@ -44,13 +44,27 @@ class Hero:
 #     def buy(self,inventory):
 #         self.inventory=inventory
 def isvalid(email,password):
-    if " " in email():
+    if " " in email:
         return"There should be no space"
     if "@" not in email:
         return "Not valid email"
     character=len(password)
     if character < 8:
         return"You need atleast 8 characters for your"
-    if password.isupper:
-        return "You need a upper case"
-print(isvalid("mangomustard@gmail.com","676141tungtung"))
+    upper=0
+    lower=0
+    for i in password:
+        if i.upper():
+            upper+=1
+        if upper == 0:
+            return "You need a upper case letter"
+        if i.lower():
+            lower+=1
+        if lower==0:
+            return "You need a lower case letter"
+        
+
+    
+            
+
+print(isvalid("Something1234@gmail.com","12355abc"))
