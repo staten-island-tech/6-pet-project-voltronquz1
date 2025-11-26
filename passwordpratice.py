8b1d30b11d60ce457c2f803e87fecb2fa0dde459
a <justinc1127@nycstudents.net>
2025-11-26 09:57:02 -0500

    Create passwordpratice.py

---
+++ passwordpratice.py
@@ -0,0 +1,25 @@
+# def isvalid(email,password):
+#     if " " in email:
+#         return"There should be no space"
+#     if "@" not in email:
+#         return "Not valid email"
+#     character=len(password)
+#     if character < 8:
+#         return"You need atleast 8 characters for your"
+#     upper=0
+#     lower=0
+#     for i in password:
+#         if i.upper():
+#             upper+=1
+#         if upper == 0:
+#             return "You need a upper case letter"
+#         if i.lower():
+#             lower+=1
+#         if lower==0:
+#             return "You need a lower case letter"
+
+
+    
+
+
+# print(isvalid("Something1234@gmail.com","12355abc"))
