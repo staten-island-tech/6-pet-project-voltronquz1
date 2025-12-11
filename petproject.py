a878d8dafd87806f8d1fdf710d6189e9b1569489
a <justinc1127@nycstudents.net>
2025-12-11 10:15:39 -0500

    Update petproject.py

--- petproject.py
+++ petproject.py
@@ -18,7 +18,7 @@ class Hero:
         pet.sleep > 0
     #caps stats
     def statcap(pet):
-        pet.hunger=max(0,min(100,pet.hunger))
+        pet.hunger=max(0,min(10,pet.hunger))
         pet.happiness=max(0,min(100,pet.happiness))
         pet.clean =max(0,min(100,pet.clean))
         pet.thirst=max(0,min(100,pet.thirst))
@@ -36,9 +36,7 @@ class Hero:
         "Work:$0",
         "sleep:$10"
     ]
-        print(options[i])
-
-        
+        print(options)
         action=input(":").lower()
         for i in options:
             if "sleep" in action:
@@ -47,13 +45,13 @@ class Hero:
                 else:
                     self.sleep+5
             if "play" in action:
-                if self.play  money:
-                    self.play+5
+                if self.happiness  > self.money:
+                    self.happiness+5
                 else:
                     print("need more money")
             if "hydrate" in action:
-                if self.hydrate < money:
-                    self.play+5
+                if self.thirst < self.money:
+                    self.thirst+5
                 else:
                     print("need more money")
             if "clean" in action:
@@ -63,6 +61,7 @@ class Hero:
                     print("need more money")
             if "work" in action:
                 money+35
+
             if "feed" in action:
                 if self.hunger < money:
                     self.hunger+10
@@ -75,7 +74,7 @@ class Hero:
                 else:
                     print("need more money")
             else: 
-                print("Invalid move")
+                print("Invalid moobse")
                 self.ap-=1
                 if self.ap==0:
                     self.ap=3
