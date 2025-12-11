a22e9cc1c7083187d1b69aa322c46b91b2cafa79
a <justinc1127@nycstudents.net>
2025-12-10 22:16:00 -0500

    Update petproject.py

--- petproject.py
+++ petproject.py
@@ -1,17 +1,22 @@
 import random
 class Hero:
-    def _init_(pet, hunger , happiness,clean,thirst,sleepyness):
-        pet.hunger=hunger
-        pet.happiness=happiness
-        pet.clean=clean
-        pet.thirst=thirst
-        pet.sleepyness=sleepyness
+    def _init_(self):
+        self.hunger=100
+        self.thirst=100
+        self.clean=100
+        self.sleep=100
+        self.happiness=100
+        # player money
+        self.money=100
+        self.ap=3
+    #check if alive
     def alive(pet):
         pet.hunger > 0
         pet.happiness >0
         pet.clean > 0
         pet.thirst > 0
         pet.sleep > 0
+    #caps stats
     def statcap(pet):
         pet.hunger=max(0,min(100,pet.hunger))
         pet.happiness=max(0,min(100,pet.happiness))
@@ -31,7 +36,7 @@ class Hero:
     dirtyness=random.randint(5,10)
     thirstness=random.randint(5,10)
     sleepyness=random.randint(5,10)
-    def game():
+    def game(self):
         options=[
         "Play:$10",
         "Feed:$10",
@@ -40,49 +45,44 @@ class Hero:
         "Work:$0",
         "sleep:$10"
     ]
-        play=10
-        feed=10
-        hydrate=10
-        clean=10
-        work=0
-        sleep=10
         print(options)
         action=input(":").lower()
-        valid=True
-        if "sleep" in action:
-            if money < sleep:
-                print( "Need more money")
-            else:
-                sleep+5
-        if "play" in action:
-            if play < money:
-                play+5
-            else:
-                print("need more money")
-        if "hydrate" in action:
-            if hydrate < money:
-                play+5
-            else:
-                print("need more money")
-        if "clean" in action:
-            if clean < money:
-                play+5
-            else:
-                print("need more money")
-        if "work" in action:
-            money+35
-        if "feed" in action:
-            if hunger < money:
-                hunger+10
-            else:
-                print("need more money")
-        if "sleep" in action:
-            if play < money:
-                play+5
-                money= money- work
-            else:
-                print("need more money")
-        else: 
-            print("Invalid move")
-        if valid:
-            ap-1
+        for i in options:
+            if "sleep" in action:
+                if self.money < self.sleep:
+                    print( "Need more money")
+                else:
+                    self.sleep+5
+            if "play" in action:
+                if self.play < money:
+                    self.play+5
+                else:
+                    print("need more money")
+            if "hydrate" in action:
+                if self.hydrate < money:
+                    self.play+5
+                else:
+                    print("need more money")
+            if "clean" in action:
+                if self.clean < money:
+                    self.play+5
+                else:
+                    print("need more money")
+            if "work" in action:
+                money+35
+            if "feed" in action:
+                if self.hunger < money:
+                    self.hunger+10
+                else:
+                    print("need more money")
+            if "sleep" in action:
+                if self.play < money:
+                    self.play+5
+                    money= money- self.work
+                else:
+                    print("need more money")
+            else: 
+                print("Invalid move")
+                self.ap-=1
+                if self.ap==0:
+                    self.ap=3
