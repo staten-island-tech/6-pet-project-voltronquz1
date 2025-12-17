ce9677aa8de99cfddffc0a34b1fefd1870ac40ce
a <justinc1127@nycstudents.net>
2025-12-16 22:53:52 -0500

    Update petproject.py

--- petproject.py
+++ petproject.py
@@ -1,6 +1,6 @@
-import random
+
 class hero:
-    def _init_(self,hunger=100,thirst=100,clean=100,sleep=100,happiness=100,money=300,ap=3):
+    def __init__(self,hunger,thirst,clean,sleep,happiness,money):
         self.hunger=hunger
         self.thirst=thirst
         self.clean=clean
@@ -8,87 +8,104 @@ class hero:
         self.happiness=happiness
         # player money
         self.money=money
-        self.ap=ap
+
 
     #check if alive
     def alive(self):
-        self.hunger > 0
-        self.happiness >0
-        self.clean > 0
-        self.thirst > 0
-        self.sleep > 0
+        return (
+            self.hunger > 0 and
+            self.happiness >0 and
+            self.clean > 0 and
+            self.thirst > 0 and
+            self.sleep > 0
+        )
     #caps stats
     def statcap(self):
-        self.hunger=max(0,min(10,self.hunger))
+        self.hunger=max(0,min(100,self.hunger))
         self.happiness=max(0,min(100,self.happiness))
         self.clean =max(0,min(100,self.clean))
         self.thirst=max(0,min(100,self.thirst))
         self.sleep =max(0,min(100,self.sleep))
-    happinessloss=random.randint(5,10)
-    dirtyness=random.randint(5,10)
-    thirstness=random.randint(5,10)
-    sleepyness=random.randint(5,10)
-    name=input("Give me a name to your pet:  ")
+    
     
 options=[
-"Play:$10",
-"Feed:$10",
-"Hydrate:$10",
-"Clean:$10",
-"Work:$0",
-"sleep:$10"
+    "Play:$10",
+    "Feed:$10",
+    "Hydrate:$10",
+    "Clean:$10",
+    "Work:$0",
+    "sleep:$10"
 ]
 
-
-
+justin = hero(100,100,100,100,100,100)
+name=input("Give me a name for pet: ")
 def game():    
     while True:
-        hero.statcap()
-        hero.alive()
-        print(options)
         print("welcome to this game")
         print(options)
         action=input(":").lower()
-        print(f"Here are your stats:\nHunger:{hero.hunger}\nHappiness:{hero.clean}\nHere your clean:{self.dirtyness}:\nHappiness{self.happiness}\nMoney:{self.money}")
+        print(f"Here are your stats:\nHunger:{justin.hunger}\nHappiness:{justin.happiness}\nHere your clean:{justin.clean}:\nMoney:{justin.money}")
+        if action == "play":
+            if 10< justin.money:
+                justin.happiness+=20
+                justin.money-=10
+                justin.clean-=10
+                justin.hunger-=10
+                justin.sleep-=10
+            else:
+                print("need more money")
+            if action == "hydrate":
+                if 10< justin.money:
+                    justin.thirst+=20
+                    justin.clean-=10
+                    justin.sleep-=10
+                    justin.hunger-=10
+                    justin.money-=10
+                else:
+                    print("need more money")
+            if action == "clean":
+                if 10< justin.money:
+                    justin.clean +=20
+                    justin.hunger-=10
+                    justin.sleep-=10
+                    justin.thirst-=10
+                    justin.money-=10
+                else:
+                    print("need more money")
+            if action == "work":
+                if 10< justin.money:
+                    justin.money+=35
+                    justin.clean-=5
+                    justin.happiness-=10
+                    justin.hunger-=5
+                    justin.sleep-=5
+                else:
+                    print("need more money")
 
-        for i in options:
-            if "sleep" in action:
-                if self.money > self.sleep:
-                    print( "Need more money")
-                else:
-                    self.sleep+=5
-            if "play" in action:
-                if self.happiness  > self.money:
-                    self.happiness+=5
-                else:
-                    print("need more money")
-            if "hydrate" in action:
-                if self.thirst < self.money:
-                    self.thirst+=5
+            if action == "feed":
+                if 10<justin.money:
+                    justin.hunger+=20
+                    justin.clean-=5
+                    justin.money-=10
+                    justin.thirst-=5
+                    justin.happiness-=5
+                    justin.sleep-=5
                 else:
                     print("need more money")
-            if "clean" in action:
-                if self.clean < money:
-                    self.play +=5
-                else:
-                    print("need more money")
-            if "work" in action:
-                money+35
-
-            if "feed" in action:
-                if self.hunger < money:
-                    self.hunger+=10
-                else:
-                    print("need more money")
-            if "sleep" in action:
-                if self.play < money:
-                    self.play+=5
-                    money= money- self.work
-                else:
-                    print("need more money")
+            if action == "feed":
+                justin.sleep+=20
+                justin.money-=10
+                justin.hunger-=10
+                justin.thirst-=10
+                justin.happiness-=10
+                justin.clean-=10
             else: 
-                print("Invalid moobse")
-                self.ap-=1
-                if self.ap==0:
-                    self.ap=3
-    # kauhwreg = hero(100,100,100,100,0,0,15)
+                print("Invalid response")
+        justin.statcap()
+        if not justin.alive():
+            print(f"{name} died")
+            break
+
+
+
+
